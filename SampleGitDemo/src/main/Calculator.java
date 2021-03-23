487c7a25bdca28c3cf230ad33dc51edc2f99284b
Ramsha Laptop <Ramsha Laptop@LAPTOP-L0U1DHEN>
2021-03-23 10:32:27 +0530

    Added cube in master

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -26,5 +26,8 @@ public class Calculator {
 	public static void square(int num1){
 	      System.out.println(num1*num1);
 	}
+	public static void cube(int num1){
+	      System.out.println(num1*num1*num1);
+	}
 
 }
